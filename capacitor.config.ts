e5161fc762f2ebbc164fe1e30df75741fdd5bb68
DevReev <munnalumpy@gmail.com>
2022-01-08 16:43:12 +0530

    Attempt to add Ionic for IOS app

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.veermaruthesh.netflix.com',
+  appName: 'netflix',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import Row from './Row';
 import requests from './requests';
 import Banner from './Banner';
 import Nav from './Nav';
+import { IonApp} from '@ionic/react';
 
 function App() {
   return (
-    <div className="app">
-      {/* <h1>Netflix</h1> */}
-      <Nav />
-      <Banner />
-      <Row title='Trending' fetchUrl={requests.fetchTrending} isLarge/>
-      <Row title='Top Rated' fetchUrl={requests.fetchTopRated}/>
-      <Row title='Action' fetchUrl={requests.fetchActionMovies}/>
-      <Row title='Comedy' fetchUrl={requests.fetchComedyMovies}/>
-      <Row title='Horror' fetchUrl={requests.fetchHorrorMovies}/>
-      <Row title='Romance' fetchUrl={requests.fetchRomanceMovies}/>
-      <Row title='Mystery' fetchUrl={requests.fetchMystery}/>
-      <Row title='SciFi' fetchUrl={requests.fetchSciFi}/>
-      <Row title='Western' fetchUrl={requests.fetchWestern}/>
-      <Row title='Animation' fetchUrl={requests.fetchAnimation}/>
-      <Row title='TV' fetchUrl={requests.fetchTV}/>
-    </div>
+    <IonApp>
+      <div className="app">
+        {/* <h1>Netflix</h1> */}
+        <Nav />
+        <Banner />
+        <Row title='Trending' fetchUrl={requests.fetchTrending} isLarge/>
+        <Row title='Top Rated' fetchUrl={requests.fetchTopRated}/>
+        <Row title='Action' fetchUrl={requests.fetchActionMovies}/>
+        <Row title='Comedy' fetchUrl={requests.fetchComedyMovies}/>
+        <Row title='Horror' fetchUrl={requests.fetchHorrorMovies}/>
+        <Row title='Romance' fetchUrl={requests.fetchRomanceMovies}/>
+        <Row title='Mystery' fetchUrl={requests.fetchMystery}/>
+        <Row title='SciFi' fetchUrl={requests.fetchSciFi}/>
+        <Row title='Western' fetchUrl={requests.fetchWestern}/>
+        <Row title='Animation' fetchUrl={requests.fetchAnimation}/>
+        <Row title='TV' fetchUrl={requests.fetchTV}/>
+      </div>
+    </IonApp>
   );
 }
 
